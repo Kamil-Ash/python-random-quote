0eea3f515533f3360c76354eaad7d7da742fa617
Kamil Ashraf Bhat <kamilashrafbhat@gmail.com>
2022-03-02 09:26:20 +0530

    add more functionality to primary

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,14 @@
 import random
-def primary():
+def primary(num):
     print("Keep it logically awesome.")
 
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
     last = len(quotes) - 1
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
+    for _ in range(num):
+        rnd = random.randint(0, last)
+        print(quotes[rnd],end='')
 
 if __name__== "__main__":
-    primary()
+    primary(2)
